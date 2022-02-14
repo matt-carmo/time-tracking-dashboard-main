e96fbac72bc4f61db0d3df39ceedb89ede34ca5a
matt-carmo <matheus_2018@hotmail.com>
2022-02-14 17:29:49 -0300

    refatoracao

--- js/script.js
+++ js/script.js
@@ -1,7 +1,65 @@
+"use strict";
+
 const title = document.querySelectorAll(".title");
 const current = document.querySelectorAll(".current");
 const previous = document.querySelectorAll(".previous");
-const activeState = document.querySelectorAll(".ulActive");
+const activeState = document.querySelectorAll(".ulActive")
+
+const hrs = 'hrs'
+
+const getPosts = async () => {
+      const response  = await fetch('./data.json')
+      const data = await response.json()
+
+      return data
+}
+
+const titles = getPosts().then(value => {
+      value.forEach( (item, i, j) => {
+         title[i].innerHTML = item.title
+      })
+
+})
+ 
+const daily =() => getPosts().then(value => {
+      value.forEach((item, i) => {
+                  current[i].innerHTML = item.timeframes.daily.current + hrs
+            
+            previous[i].innerHTML = 'Last Week - ' +    item.timeframes.daily.previous + 'hrs'
+            
+            item.timeframes.daily.previous < 1 ? previous[i].innerHTML = 'Last Week - ' +    item.timeframes.daily.current + 'hrs':0
+      })
+})
+daily()
+
+const weekly = () => getPosts().then(value => {
+      value.forEach((item, i) => {
+            current[i].innerHTML = item.timeframes.weekly.current + hrs
+            console.log(item.timeframes.weekly.previous)
+
+            previous[i].innerHTML = 'Last Week - ' +    item.timeframes.weekly.previous + 'hrs'
+      })
+})
+
+const monthly = () => getPosts().then(value => {
+      value.forEach((item, i) => {
+            current[i].innerHTML = item.timeframes.monthly.current + hrs
+            
+            previous[i].innerHTML = 'Last Week - ' +    item.timeframes.monthly.previous + 'hrs'
+
+         
+      })
+})
+
+
+
+
+
+
+;
+
+
+
 const lastWeek = "Last week - ";
 
 const rm = () => activeState.forEach((rm) => rm.classList.remove("active"));
@@ -13,60 +71,16 @@ const list = activeState.forEach((active) => {
         active.classList.add("active");
         
         if (active.textContent === "Daily") {
-            dailyPress();
+            daily()
         }
         if (active.textContent === "Weekly") {
-            weeklyPress();
+            weekly()
         }
         if (active.textContent === "Monthly") {
-            monthly();
+            monthly()
         }
     });
 });
 
-const xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        const myObj = JSON.parse(this.responseText);        
 
 
-        myObj.forEach(() => {
-            for (let i = 0; i < title.length; i++) {
-                title[i].innerHTML = myObj[i].title;
-            }
-        });
-
-        dailyPress = () => {
-            current.forEach((element) => {
-                for (let j = 0; j < current.length; j++) {
-                    current[j].innerHTML =
-                        myObj[j].timeframes.daily.current + "hrs";
-                    previous[j].innerHTML =
-                        lastWeek + myObj[j].timeframes.daily.previous + "hrs";
-                }
-            });
-        };
-        dailyPress()
-        
-
-        weeklyPress = () => {
-            for (let j = 0; j < current.length; j++) {
-                current[j].innerHTML =
-                    myObj[j].timeframes.weekly.current + "hrs";
-                previous[j].innerHTML =
-                    lastWeek + myObj[j].timeframes.weekly.previous + "hrs";
-            }
-        };
-        monthly = () => {
-            for (let j = 0; j < current.length; j++) {
-                current[j].innerHTML =
-                    myObj[j].timeframes.monthly.current + "hrs";
-                previous[j].innerHTML =
-                    lastWeek + myObj[j].timeframes.monthly.previous + "hrs";
-            }
-        };
-    }
-};
-
-xmlhttp.open("GET", "js/data.json", true);
-xmlhttp.send();
